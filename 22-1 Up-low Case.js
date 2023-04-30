ae43c44732529e90a42f35df203730731b188c2b
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-04-30 21:53:06 +0600

    22-1 Up-low Case

---
+++ 22-1 Up-low Case.js
@@ -0,0 +1,21 @@
+const fullName = "Soyed Muhammad Muntasir Billah Armaan Hossain";
+const userName = "SOYED MUHAMMAD MUNTASIR BILLAH ARMAAN HOSSAIN";
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.toLowerCase());
+console.log(fullName.toUpperCase());
+
+if (fullName === userName) {
+  console.log("Valid User");
+} else {
+  console.log("invalid user.");
+}
+
+
+
+if (fullName.toLowerCase === userName.toLowerCase) {
+    console.log("Valid User");
+  } else {
+    console.log("invalid user.");
+  }
+  
