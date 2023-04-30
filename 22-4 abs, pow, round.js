b01b30f1678b6f92f86a51a01a226d9293d57cfb
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-04-30 21:53:57 +0600

    22-4 abs, pow, round

---
+++ 22-4 abs, pow, round.js
@@ -0,0 +1,25 @@
+const num_power = Math.pow(2, 10);
+// console.log(num_power);
+
+const your_balance = 20000;
+const make_over_cost = 27000;
+
+const gap = Math.abs(your_balance - make_over_cost);
+
+// if (gap <= 5000) {
+//   console.log("you can do your make over with us....");
+// } else {
+//   console.log("please leave .... you can not do your make over with us....");
+// }
+
+const avg_num = 26.583;
+
+// console.log(Math.round(avg_num));
+
+// console.log(Math.ceil(avg_num));
+
+// console.log(Math.floor(avg_num));
+
+for (let i = 1; i < 20; i++) {
+  console.log(Math.round(Math.random() * 6));
+}
